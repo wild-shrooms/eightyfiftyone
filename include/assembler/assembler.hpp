8aa398538b672d40d70b2ba53583239be31b5bdd
wwsmiff <arnav0872@gmail.com>
2024-08-12 21:26:06 +0530

    Add argument parsing to assembler

---
+++ include/assembler/assembler.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <filesystem>
+
+namespace assembler {
+
+constexpr std::string_view help_message{
+    "Assembler for the 8051 architecture\n"
+    "Usage: 8051-assembler [file] [options]\n"
+    "Arguments:\n"
+    "\t-h, --help\t\tDisplay the help message.\n"
+    "\t-o, --output\t\tOutput binary to specified file.\n"};
+
+void display_help_message();
+void assemble(const std::filesystem::path &in);
+void assemble(const std::filesystem::path &in,
+              const std::filesystem::path &out);
+} // namespace assembler
